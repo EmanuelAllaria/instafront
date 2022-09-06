62b56e570c5e437ec17b0c4195a3c23ab859b74f
EmanuelAllaria <allariaemanuel@gmail.com>
2022-09-05 21:25:28 -0300

    insta

---
+++ js/js.js
@@ -0,0 +1,10 @@
+corazon = document.querySelector(".corazon");
+emoji = document.querySelector(".emoji");
+
+corazon.addEventListener("click", function(){
+    corazon.src = "img/cora_rojo.png"
+})
+
+emoji.addEventListener("click", function(){
+    emoji.src = "img/emoji-feliz.png"
+})
